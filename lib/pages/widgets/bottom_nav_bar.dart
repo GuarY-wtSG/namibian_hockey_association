f25cb11ca307c5cc7774bae28edbd7b78082b294
GuarY-wtSG <mailmkay5@gmail.com>
2025-05-22 22:48:11 +0200

    Redisign of the Bottom navigation bar. setting up the complete Redisign of the landing page

---
+++ lib/pages/widgets/bottom_nav_bar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class CustomBottomNav extends StatelessWidget {
+  void Function(int)? onTabChange;
+  CustomBottomNav({super.key, required this.onTabChange});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: GNav(
+        color: Colors.grey[400],
+        activeColor: Colors.grey.shade700,
+        tabActiveBorder: Border.all(color: Colors.white),
+        tabBackgroundColor: Colors.grey.shade100,
+        mainAxisAlignment: MainAxisAlignment.center,
+        tabBorderRadius: 16,
+        onTabChange: (value) => onTabChange!(value),
+        tabs: const [
+          // navigate to the events that the team is signed up for
+          GButton(icon: Icons.newspaper, text: 'News'),
+          // navigate to team management
+          GButton(icon: Icons.group, text: 'Manage Team'),
+          // navigate to event where you can make entries or join
+          GButton(icon: Icons.login, text: 'Events'),
+        ],
+      ),
+    );
+  }
+}
