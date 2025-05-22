4abc109cbb165d0073b333eca6aa1d0cff058d13
GuarY-wtSG <mailmkay5@gmail.com>
2025-05-22 23:27:48 +0200

    Updated Landing page with better naviagtion between display pages. The old landing page is at the bottom. All thet's is to uncomment it and comment the top part out.

--- lib/pages/landing.dart
+++ lib/pages/landing.dart
@@ -1,4 +1,127 @@
 import 'package:flutter/material.dart';
+import 'package:nho_app/pages/widgets/bottom_nav_bar.dart';
+// importing main pages
+import 'events.dart';
+import 'news.dart';
+import 'team_management.dart';
+
+class Landing extends StatefulWidget {
+  const Landing({super.key});
+
+  @override
+  State<Landing> createState() => _LandingState();
+}
+
+class _LandingState extends State<Landing> {
+  // int to keep track of selected index for bottm nav
+  int _selectedIndex = 0;
+
+  // method to update index
+  // when user taps bottom bar
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  //pages to diplay
+  final List<Widget> _pages = [
+    // notifications and current upcoming events
+    const News(),
+    // team management
+    const TeamManage(),
+    // enter a new event
+    const Events(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: CustomBottomNav(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Builder(
+          builder:
+              (context) => IconButton(
+                icon: const Icon(Icons.menu, color: Colors.black),
+                onPressed: () {
+                  Scaffold.of(context).openDrawer();
+                },
+              ),
+        ),
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.grey[800],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                // logo
+                DrawerHeader(
+                  child: Icon(
+                    Icons.sports_hockey,
+                    color: Colors.green[400],
+                    size: 140,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Divider(color: Colors.grey[700]),
+                ),
+
+                // other pages
+                const Padding(
+                  padding: EdgeInsets.only(left: 25.0),
+                  child: ListTile(
+                    leading: Icon(Icons.home, color: Colors.white),
+                    title: Text('Home', style: TextStyle(color: Colors.white)),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 25.0),
+                  child: ListTile(
+                    leading: Icon(Icons.info, color: Colors.white),
+                    title: Text('About', style: TextStyle(color: Colors.white)),
+                  ),
+                ),
+              ],
+            ),
+
+            // spacing logout to the bottom of the page
+            const Padding(
+              padding: EdgeInsets.only(left: 25.0, bottom: 25),
+              child: ListTile(
+                leading: Icon(Icons.logout, color: Colors.white),
+                title: Text('Logout', style: TextStyle(color: Colors.white)),
+              ),
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: const Color.fromARGB(255, 214, 235, 194),
+      body: _pages[_selectedIndex],
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* import 'package:flutter/material.dart';
 
 class Landing extends StatefulWidget {
   const Landing({super.key});
@@ -162,3 +285,4 @@ class _LandingState extends State<Landing> {
     );
   }
 }
+ */
