b8c75d436ed12b35e17e7aa6072c1e38f940a5e8
GuarY-wtSG <mailmkay5@gmail.com>
2025-05-29 18:17:16 +0200

    Url fix Allowing for redirection once a news tile is tapped

--- lib/pages/widgets/news_tile.dart
+++ lib/pages/widgets/news_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../models/news_model.dart';
 
@@ -7,50 +8,62 @@ class NewsTile extends StatelessWidget {
   News news;
   NewsTile({super.key, required this.news});
 
+  void _launchURL() async {
+    Uri url = Uri.parse(news.url);
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(left: 25),
-      width: 280,
-      decoration: BoxDecoration(
-        color: Colors.grey[100],
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Column(
-        children: [
-          //news pic
-          ClipRRect(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(12),
-              topRight: Radius.circular(12),
-            ),
-            child: Image.asset(
-              news.imagePath,
-              // this is to handle image render issues
-              errorBuilder: (context, error, StackTrace) {
-                return Icon(Icons.image_not_supported);
-              },
-            ),
-          ),
-          SizedBox(height: 20),
-
-          //News description
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10.0),
-            child: Text(
-              news.title,
-              style: TextStyle(
-                color: Colors.blueGrey,
-                fontWeight: FontWeight.bold,
-                fontSize: 24,
+    return GestureDetector(
+      onTap: _launchURL,
+      child: Container(
+        margin: EdgeInsets.only(left: 25),
+        width: 280,
+        decoration: BoxDecoration(
+          color: Colors.grey[100],
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Column(
+          children: [
+            //news pic
+            ClipRRect(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(12),
+                topRight: Radius.circular(12),
+              ),
+              child: Image.asset(
+                news.imagePath,
+                // this is to handle image render issues
+                errorBuilder: (context, error, StackTrace) {
+                  return Icon(Icons.image_not_supported);
+                },
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 20.0),
-            child: Text(news.description),
-          ),
-        ],
+            SizedBox(height: 20),
+
+            //News description
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Text(
+                news.title,
+                style: TextStyle(
+                  color: Colors.blueGrey,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: Text(news.description),
+            ),
+          ],
+        ),
       ),
     );
   }
