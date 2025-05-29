bc27bec6860d30546a359b3d6cb7ea522291c77b
GuarY-wtSG <mailmkay5@gmail.com>
2025-05-29 21:12:03 +0200

    Setting up Firebase I followed this short tutorial on how to do it https://www.youtube.com/watch?v=FYcYVkTowRs

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:nho_app/pages/login.dart';
 import 'package:nho_app/pages/landing.dart';
@@ -7,7 +9,25 @@ import 'package:nho_app/pages/signup.dart';
 import 'package:nho_app/pages/player_registration.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: "AIzaSyB_V9gBFzFB_srRrV2EyzWltf0oBxZG4qs",
+        authDomain: "nho-85f86.firebaseapp.com",
+        projectId: "nho-85f86",
+        storageBucket: "nho-85f86.firebasestorage.app",
+        messagingSenderId: "1022543168990",
+        appId: "1:1022543168990:web:a52f7f1e7d39fd6c73494f",
+        measurementId: "G-KH3TH6SX78",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MainApp());
 }
 
